05d9365cf8e3eed1641e1bbde62e73f98a88416c
Максим <@DESKTOP-9JOEI7F>
2023-03-22 16:32:37 +0200

    Добавьте файлы проекта.

---
+++ Cyclic calculation/Cyclic calculation.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void cycle()
+{
+    int last_num, sum = 0;
+    std::cout << "Enter last number: ";
+    std::cin >> last_num;
+    std::cout << std::endl;
+    for (int i = 0; i <= last_num; i++)
+    {
+        if (i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    std::cout << "Sum = " << sum << std::endl;
+
+}
+
+
+int main()
+{
+    cycle();
+    std::cout << "Hello World!\n";
+}
